fbe8e1106c637cb1395b12797fbede621b2d2ae1
sbailie <sbailie@deloitte.co.uk>
2021-12-01 11:25:43 +0000

    Inital project and very quick and dirty [DAY 1 - 2 STARS]

---
+++ day1.py
@@ -0,0 +1,64 @@
+with open('inputs/1-0.1') as file:
+    lines = file.readlines()
+    lines = [int(line.rstrip()) for line in lines]
+    #print(lines)
+
+count = 0
+prev = lines[0]
+for val in lines:
+    if val > prev:
+        count += 1
+    prev = val
+
+print("Test Input (7): " + str(count))
+
+###################################
+
+with open('inputs/1-1') as file:
+    lines = file.readlines()
+    lines = [int(line.rstrip()) for line in lines]
+    #print(lines)
+
+count = 0
+prev = lines[0]
+for val in lines:
+    if val > prev:
+        count += 1
+    prev = val
+
+
+print("Test Input (1228): " + str(count))
+
+###################################
+
+with open('inputs/1-0.1') as file:
+    lines = file.readlines()
+    lines = [int(line.rstrip()) for line in lines]
+    #print(lines)
+
+count = 0
+prev = lines[0] + lines[1] + lines[2]
+for i in range(1, len(lines) - 2):
+    val = lines[i] + lines[i+1] + lines[i+2]
+    if val > prev:
+        count += 1
+    prev = val
+
+print("Test Input (5): " + str(count))
+
+###################################
+
+with open('inputs/1-1') as file:
+    lines = file.readlines()
+    lines = [int(line.rstrip()) for line in lines]
+    #print(lines)
+
+count = 0
+prev = lines[0] + lines[1] + lines[2]
+for i in range(1, len(lines) - 2):
+    val = lines[i] + lines[i+1] + lines[i+2]
+    if val > prev:
+        count += 1
+    prev = val
+
+print("Test Input (1257): " + str(count))
