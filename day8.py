807ce7b32dc9ee51a0893b3a31f1a59c8b53552d
Steven Bailie <srjbailie@gmail.com>
2021-12-08 22:40:34 +0000

    Seven Segment Displays - That one was a tough one to debug an issue. Could be much more elegant with a mapping rather than nested if but I'm tired.. [DAY 8 - 2 STARS]

--- day8.py
+++ day8.py
@@ -16,20 +16,7 @@
 # .    f  e    f  .    f  e    f  .    f
 #  gggg    gggg    ....    gggg    gggg
 #
-#
-# Seven Segment Figits
-# 0: abcefg
-# 1: cf
-# 2: acdeg
-# 3: acdfg
-# 4: bcdf
-# 5: abdfg
-# 6: abdefg
-# 7: acf
-# 8: abcdefg
-# 9: abcdfg
-
-dictionary = {
+seven_segment_dictionary = {
     '0': 'abcefg',  # 6 digits
     '1': 'cf',  # 2 digits
     '2': 'acdeg',  # 5 digits
@@ -53,8 +40,10 @@ def process_seven_segment_notes(notes):
     processed = []
     for line in notes:
         signals = line.split('|')[0].strip().split(' ')
+        alphabetised_signals = ["".join(sorted(i)) for i in signals]
         output = line.split('|')[1].strip().split(' ')
-        buffer = {'signals': signals, 'output': output}
+        alphabetised_output = ["".join(sorted(i)) for i in output]
+        buffer = {'signals': alphabetised_signals, 'output': alphabetised_output}
         processed.append(buffer)
     return processed
 
@@ -81,6 +70,79 @@ def count_1s_4s_7s_8s_in_outputs(notes):
     return sum(tally)
 
 
+def count_overlapping_characters(a, b):
+    count = 0
+    for char in a:
+        if char in b:
+            count += 1
+    return count
+
+
+# Part 2
+def solve_and_sum_outputs(notes):
+    total = 0
+
+    for note in notes:
+        note['solved'] = [0] * 10
+
+        # Solve for 1's and 4's
+        for value in note['signals']:
+            if len(value) == 2:
+                # 2 Segments Lit Up = '1'
+                note['solved'][1] = value
+            elif len(value) == 4:
+                # 4 Segments Lit Up = '4'
+                note['solved'][4] = value
+            elif len(value) == 3:
+                # 3 Segments Lit Up = '7'
+                note['solved'][7] = value
+            elif len(value) == 7:
+                # 7 Segments Lip Up = '8'
+                note['solved'][8] = value
+
+        def solve_based_on_overlap(test_value):
+            overlap_with_1 = count_overlapping_characters(test_value, note['solved'][1])
+            overlap_with_4 = count_overlapping_characters(test_value, note['solved'][4])
+            if len(test_value) in [2, 4, 3, 7]:
+                # Already solved
+                return 'pre-solved'
+            elif len(test_value) == 6:
+                if overlap_with_1 == 1:
+                    return 6
+                elif overlap_with_1 == 2:
+                    if overlap_with_4 == 3:
+                        return 0
+                    elif overlap_with_4 == 4:
+                        return 9
+            elif len(test_value) == 5:
+                if overlap_with_4 == 4:
+                    return 9
+                elif overlap_with_4 == 2:
+                    return 2
+                elif overlap_with_4 == 3:
+                    if overlap_with_1 == 2:
+                        return 3
+                    elif overlap_with_1 == 1:
+                        return 5
+
+        # Solve all digits
+        for value in note['signals']:
+            if len(value) != (2 or 4):
+                # If not 2 or 4 Segments Lit Up (As '1' and '4' already solved)
+                # Otherwise can be solved:
+                solved_index = solve_based_on_overlap(value)
+                if solved_index != 'pre-solved':
+                    note['solved'][solved_index] = value
+
+        # Decode output
+        output_string = ''
+        for value in note['output']:
+            output_string += str(note['solved'].index(value))
+        total += int(output_string)
+
+    return total
+
+
 ###########################################
 
 seven_segment_notes = process_seven_segment_notes(ingest_list_of_lines('inputs/8-0.1'))
@@ -98,5 +160,23 @@ print("Part 1 Test Input (26): " + str(unique_digits))
 seven_segment_notes = process_seven_segment_notes(ingest_list_of_lines('inputs/8-1'))
 unique_digits = count_1s_4s_7s_8s_in_outputs(seven_segment_notes)
 print("Part 1 Real Input (440): " + str(unique_digits))
+
+###########################################
+
+seven_segment_notes = process_seven_segment_notes(ingest_list_of_lines('inputs/8-0.1'))
+output_sum = solve_and_sum_outputs(seven_segment_notes)
+print("Part 2 Mini Test Input (5353): " + str(output_sum))
+
+###########################################
+
+seven_segment_notes = process_seven_segment_notes(ingest_list_of_lines('inputs/8-0.2'))
+output_sum = solve_and_sum_outputs(seven_segment_notes)
+print("Part 2 Test Input (61229): " + str(output_sum))
+
+###########################################
+
+seven_segment_notes = process_seven_segment_notes(ingest_list_of_lines('inputs/8-1'))
+output_sum = solve_and_sum_outputs(seven_segment_notes)
+print("Part 2 Real Input (1046281): " + str(output_sum))
 
 ###########################################
