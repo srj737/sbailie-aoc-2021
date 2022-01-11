afc147313fad0228fbed7c7d4568da2150032527
Steven Bailie <sbailie@gmail.com>
2022-01-11 13:14:05 +0100

    [DAY 21 - 2 STARS] Dirac (properly) dice. Needed help from subreddit solutions for inspiration. My recursive function was correct but just needed 'functools cache' memoization in order to run in any reasonable length of time

--- day21.py
+++ day21.py
@@ -1,5 +1,8 @@
+import functools
+import itertools
 
-def play_dirac_dice_game(positions=[0, 0], target_score=1000, board_size=10, die=[100, False]):
+
+def play_dirac_dice_game(positions=[0, 0], target_score=1000, board_size=10):
     scores = [0, 0]
     roll = 0
     player = 0
@@ -27,6 +30,25 @@ def roll_deterministic_die(last, sides=100):
     return current
 
 
+@functools.lru_cache(maxsize=None)
+def play_dirac_dice_game_part2(pos_1, pos_2, score_1=0, score_2=0, target_score=21, board_size=10):
+    wins_1 = wins_2 = 0
+    for roll_1, roll_2, roll_3 in itertools.product((1, 2, 3), (1, 2, 3), (1, 2, 3)):
+        move = roll_1 + roll_2 + roll_3
+        next_pos = ((pos_1 - 1 + move) % board_size) + 1
+        new_score_1 = score_1 + next_pos
+        new_pos_1 = next_pos
+        if new_score_1 >= target_score:
+            # Just rolled won
+            wins_1 += 1
+        else:
+            # Not winner, so next player goes
+            sub_wins_2, sub_wins_1 = play_dirac_dice_game_part2(pos_2, new_pos_1, score_2, new_score_1)
+            wins_1 += sub_wins_1
+            wins_2 += sub_wins_2
+    return wins_1, wins_2
+
+
 ###########################################
 
 scores, rolls = play_dirac_dice_game([4, 8])
@@ -34,7 +56,17 @@ print("Part 1 - Test Input (739785): " + str(min(scores) * rolls))
 
 ###########################################
 
-scores, rolls1 = play_dirac_dice_game([7, 3])
-print("Part 1 - Real Input (551901): " + str(min(scores) * rolls1))
+scores, rolls = play_dirac_dice_game([7, 3])
+print("Part 1 - Real Input (551901): " + str(min(scores) * rolls))
+
+###########################################
+
+player_wins = play_dirac_dice_game_part2(4, 8)
+print("Part 2 - Test Input (444356092776315): " + str(max(player_wins)))
+
+###########################################
+
+player_wins = play_dirac_dice_game_part2(7, 3)
+print("Part 2 - Real Input (272847859601291): " + str(max(player_wins)))
 
 ###########################################
